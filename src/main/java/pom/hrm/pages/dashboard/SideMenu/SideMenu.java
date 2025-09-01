425f74c34623f710df83ac629d1c7ce0924e1a7c
ABRAHAM <abraham7111997@gmail.com>
2025-09-02 00:20:47 +0530

    Commit for Side Menu

---
+++ src/main/java/pom/hrm/pages/dashboard/SideMenu/SideMenu.java
@@ -0,0 +1,72 @@
+package pom.hrm.pages.dashboard.SideMenu;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import utilities.WaitUtils;
+
+public class SideMenu {
+	
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	//Constructor
+	public SideMenu(WebDriver driver) {
+		this.driver = driver;
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	}
+	
+	//Locators
+	By optionAdmin = By.xpath("//span[text()='Admin']");
+	By optionPIM = By.xpath("//span[text()='PIM']");
+	By optionLeave = By.xpath("//span[text()='Leave']");
+	By optionRecruitment = By.xpath("//span[text()='Recruitment']");
+	By optionMyInfo = By.xpath("//span[text()='My Info']");
+	By dashboardTitle = By.xpath("//h6[text()='Dashboard']");
+	
+	//Action
+	public void clickOptionAdmin() {
+		driver.findElement(optionAdmin).click();
+	}
+	public void clickOptionPIM() {
+		driver.findElement(optionPIM).click();
+	}
+	public void clickOptionLeave() {
+		driver.findElement(optionLeave).click();
+	}
+	public void clickOptionRecruitment() {
+		driver.findElement(optionRecruitment).click();
+	}
+	public void clickOptionMyInfo() {
+		driver.findElement(optionMyInfo).click();
+	}
+
+	public void performSideMenu(
+			boolean isOptionAdmin,
+			boolean isOptionPIM,
+			boolean isOptionLeave,
+			boolean isOptionRecruitment,
+			boolean isOptionMyInfo) {
+		
+		WaitUtils waitUtils = new WaitUtils(driver);
+		waitUtils.waitForVisibility(dashboardTitle);
+		if(isOptionAdmin) {
+			clickOptionAdmin();
+		}
+		if(isOptionPIM) {
+			clickOptionPIM();
+		}
+		if(isOptionLeave) {
+			clickOptionLeave();
+		}
+		if(isOptionRecruitment) {
+			clickOptionRecruitment();
+		}
+		if(isOptionMyInfo) {
+			clickOptionMyInfo();
+		}
+	}
+}
--- src/test/java/pom/hrm/tests/Login/LoginTest.java
+++ src/test/java/pom/hrm/tests/Login/LoginTest.java
@@ -1,23 +1,25 @@
 package pom.hrm.tests.Login;
 
-import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import pom.hrm.dataProvider.LoginTestDataProvider;
 import pom.hrm.base.basePage;
+import pom.hrm.dataProvider.LoginTestDataProvider;
 import pom.hrm.pages.Login.LoginPage;
+import pom.hrm.pages.dashboard.SideMenu.SideMenu;
 import pom.hrm.utils.ConfigReader;
 
 public class LoginTest extends basePage {
 
     private LoginPage loginPage;
+    private SideMenu sideMenuPage;
 
     @BeforeTest
     public void setUp() {
         initializeBrowser(ConfigReader.getProperty("browser")); // loads config inside PageBase
         loginPage = new LoginPage(getDriver());
+        sideMenuPage = new SideMenu(getDriver());
     }
 
     @AfterTest
@@ -30,6 +32,11 @@ public class LoginTest extends basePage {
         loginPage.performLoginPage(true, username, password);      
 
     }
+    @Test(priority = 2)
+    public void verifySideMenu(){
+    	sideMenuPage.performSideMenu(true, true, true, true, true);
+    }
+    
     
 //  String actualError = loginPage.getErrorMessage();
 //  String expectedError = "Invalid credentials";
